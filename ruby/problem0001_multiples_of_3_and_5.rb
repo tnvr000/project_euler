edf0111c2afa8fa2b761fd4b711c3c43efab999c
Tanveer Ahmad Khan <tanveerk@mindfiresolutions.com>
2021-12-02 12:10:49 +0530

    completed problem 1 in ruby

---
+++ ruby/problem0001_multiples_of_3_and_5.rb
@@ -0,0 +1,7 @@
+# Sum of all the numbers that are multiples of 3 or 5 below 1000
+sum =  0
+(1...1000).each do |number|
+	sum += number if number % 3 == 0 || number % 5 == 0
+end
+puts sum
+# testing git somethings wrong
