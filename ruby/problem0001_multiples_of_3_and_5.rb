28056b52227f6b152d39dbb434d8ad6ea19a2262
Tanveer Ahmad Khan <tanveerk@mindfiresolutions.com>
2021-12-02 13:17:04 +0530

    completed problem 2 in ruby

--- ruby/problem0001_multiples_of_3_and_5.rb
+++ ruby/problem0001_multiples_of_3_and_5.rb
@@ -1,7 +1,17 @@
-# Sum of all the numbers that are multiples of 3 or 5 below 1000
-sum =  0
+# frozen_string_literal: true
+
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+# The sum of these multiples is 23.
+# Find the sum of all the multiples of 3 or 5 below 1000.
+
+sum1 = 0
 (1...1000).each do |number|
-	sum += number if number % 3 == 0 || number % 5 == 0
+  sum1 += number if (number % 3).zero? || (number % 5).zero?
 end
-puts sum
-# testing git somethings wrong
+puts sum1
+
+sum2 = (1...1000).select do |number|
+  (number % 3).zero? || (number % 5).zero?
+end.sum
+
+puts sum2
