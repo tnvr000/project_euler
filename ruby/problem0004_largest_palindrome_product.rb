7156213b0dc34d27712e80a9c904c324611b7d69
Tanveer Ahmad Khan <tanveerk@mindfiresolutions.com>
2021-12-08 17:31:10 +0530

    benchmarked largest palindrome

--- ruby/problem0004_largest_palindrome_product.rb
+++ ruby/problem0004_largest_palindrome_product.rb
@@ -9,6 +9,8 @@
 # two 2-digit numbers is 9009 = 91 × 99.
 # Find the largest palindrome made from the product of two 3-digit numbers.
 
+require 'benchmark'
+
 # add custom methods to integer class
 class Integer
   # returns true if the number read same both forward and backward
@@ -41,7 +43,11 @@ def palindromes(range)
   palindromes
 end
 
-def largest_palindrome
+def largest_palindrome1
+  palindromes((900..999)).max
+end
+
+def largest_palindrome2
   (0..9).reverse_each do |b|
     (0..9).reverse_each do |c|
       palindrome = 11 * (81_819 + (910 * b) + (100 * c)) # 81_819 = 9019 * 9 because a = 9
@@ -52,6 +58,9 @@ def largest_palindrome
   end
 end
 
-puts palindromes((900..999)).max
+Benchmark.bmbm do |x|
+  x.report('brute') { largest_palindrome1 }
+  x.report('formula') { largest_palindrome2 }
+end
 
-puts largest_palindrome
+puts largest_palindrome2
